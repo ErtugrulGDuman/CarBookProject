7b81cd09e70f9cba5d3bc8696d6ad4918b5b1522
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-10-10 23:12:56 +0300

    ...

---
+++ Infrastructure/CarBookProject.Persistence/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using CarBookProject.Application.Interfaces;
+using CarBookProject.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly CarBookContext _context;
+        public Task CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetByFilterAsync(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
