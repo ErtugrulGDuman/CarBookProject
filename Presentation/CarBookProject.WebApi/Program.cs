fd2e0992a9bc86200bfb5d9073798328571f3320
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-10-17 00:00:53 +0300

    Brand CQRS

--- Presentation/CarBookProject.WebApi/Program.cs
+++ Presentation/CarBookProject.WebApi/Program.cs
@@ -1,4 +1,6 @@
 using CarBookProject.Application.Features.CQRS.Handlers.AboutHandler;
+using CarBookProject.Application.Features.CQRS.Handlers.BannerHandlers;
+using CarBookProject.Application.Features.CQRS.Handlers.BrandHandlers;
 using CarBookProject.Application.Interfaces;
 using CarBookProject.Persistence.Context;
 using CarBookProject.Persistence.Repositories;
@@ -15,6 +17,18 @@ builder.Services.AddScoped<GetAboutByIdQueryHandler>();
 builder.Services.AddScoped<CreateAboutCommandHandler>();
 builder.Services.AddScoped<UpdateAboutCommandHandler>();
 builder.Services.AddScoped<RemoveAboutCommandHandler>();
+
+builder.Services.AddScoped<GetBannerQueryHandler>();
+builder.Services.AddScoped<GetBannerByIdQueryHandler>();
+builder.Services.AddScoped<CreateBannerCommandHandler>();
+builder.Services.AddScoped<UpdateBannerCommandHandler>();
+builder.Services.AddScoped<RemoveBannerCommandHandler>();
+
+builder.Services.AddScoped<GetBrandQueryHandler>();
+builder.Services.AddScoped<GetBrandByIdQueryHandler>();
+builder.Services.AddScoped<CreateBrandCommandHandler>();
+builder.Services.AddScoped<UpdateBrandCommandHandler>();
+builder.Services.AddScoped<RemoveBrandCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
