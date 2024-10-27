c28aa732bbc837a557fed2bbc95702c99d597205
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-10-27 22:39:11 +0300

    Location MediatR and Api's

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/LocationHandlers/GetPricingByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using CarBookProject.Application.Features.Mediator.Queries.LocationQueries;
+using CarBookProject.Application.Features.Mediator.Results.LocationResults;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.LocationHandlers
+{
+    public class GetPricingByIdQueryHandler : IRequestHandler<GetLocationByIdQuery, GetLocationByIdQueryResult>
+    {
+        private readonly IRepository<Location> _repository;
+        public GetPricingByIdQueryHandler(IRepository<Location> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetLocationByIdQueryResult> Handle(GetLocationByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.Id);
+            return new GetLocationByIdQueryResult
+            {
+                LocationId = values.LocationId,
+                Name = values.Name
+            };
+        }
+    }
+}
