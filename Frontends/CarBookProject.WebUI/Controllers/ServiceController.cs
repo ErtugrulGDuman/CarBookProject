f097904c1a4b478377c5f99879e7ed4e6c831519
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-11-01 22:33:57 +0300

    Testimonial MerdiatR and Service...

---
+++ Frontends/CarBookProject.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,30 @@
+﻿using CarBookProject.Dto.ServiceDtos;
+using CarBookProject.Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookProject.WebUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7265/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
