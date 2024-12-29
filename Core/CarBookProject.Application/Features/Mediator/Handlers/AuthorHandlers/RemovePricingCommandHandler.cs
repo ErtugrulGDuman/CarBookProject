197ee549eec327f31ebc3480f5296f856f620cff
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-12-29 22:40:56 +0300

    Author MediatR and Controller

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/AuthorHandlers/RemovePricingCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CarBookProject.Application.Features.Mediator.Commands.AuthorCommands;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.AuthorHandlers
+{
+    public class RemovePricingCommandHandler : IRequestHandler<RemoveAuthorCommand>
+    {
+        private readonly IRepository<Author> _repository;
+
+        public RemovePricingCommandHandler(IRepository<Author> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(RemoveAuthorCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.Id);
+            await _repository.RemoveAsync(value);
+        }
+    }
+}
