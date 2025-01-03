c2ebbb9c77348f83f6871c28221db96185232a37
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2025-01-03 21:41:18 +0300

    ...

--- Frontends/CarBookProject.WebUI/Controllers/ServiceController.cs
+++ Frontends/CarBookProject.WebUI/Controllers/ServiceController.cs
@@ -16,6 +16,8 @@ namespace CarBookProject.WebUI.Controllers
 
         public async Task<IActionResult> Index()
         {
+            ViewBag.v1 = "Hizmetler";
+            ViewBag.v2 = "Hizmetlerimiz";
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7265/api/Services");
             if (responseMessage.IsSuccessStatusCode)
