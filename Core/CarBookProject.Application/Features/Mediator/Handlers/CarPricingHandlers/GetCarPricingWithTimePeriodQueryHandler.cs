d66ea62e51160fffb3ee8ee166c8419453ceee04
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2025-08-28 23:23:35 +0300

    ...

--- Core/CarBookProject.Application/Features/Mediator/Handlers/CarPricingHandlers/GetCarPricingWithTimePeriodQueryHandler.cs
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/CarPricingHandlers/GetCarPricingWithTimePeriodQueryHandler.cs
@@ -25,9 +25,9 @@ namespace CarBookProject.Application.Features.Mediator.Handlers.CarPricingHandle
                 BrandName = x.Brand,
                 Model = x.Model,
                 CoverImageUrl = x.CoverImageUrl,
-                DailyAmount = x.Amounts[0] ??0,
-                WeeklyAmount = x.Amounts[1] ?? 1,
-                MonthlyAmount = x.Amounts[2] ?? 2
+                DailyAmount = x.Amounts[0],
+                WeeklyAmount = x.Amounts[1],
+                MonthlyAmount = x.Amounts[2]
             }).ToList();
         }
     }
